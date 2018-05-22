2ca8598b1a3eaede1ff9b5cda7ac848b66bf34f0
Vanessa Toro <vanetoro84@gmail.com>
2018-05-22 01:13:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
 
   get '/friends' do
     # Write your code here!
-    @friends = ['Rachel', 'Lili', 'Yunaida', 'Juli']
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
     erb :friends
   end
